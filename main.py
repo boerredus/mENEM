fe2da176230b9576c2684c21dd00506b593278d0
João Vaz <46465813+boerredus@users.noreply.github.com>
2021-09-23 16:29:05 -0300

    Add file parsing and eval options

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
 import asyncio
+import argparse
+import io
+import os
 
 import mbsh
+import utils
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog='mbsh')
+    parser.add_argument('file', type=str, nargs='?')
+    parser.add_argument('-e', '--eval', type=str, required=False)
+
+    return parser.parse_args()
+
+
+async def parse_file(fp: io.TextIOWrapper, _mbsh: mbsh.MbSh):
+    allowed = ['EOF', 'clear', 'config', 'help', 'history']
+    lines = fp.readlines()
+
+    for line in lines:
+        line = line.strip()
+        cmd = utils.get_default(line.split(' '), 0)
+
+        if not cmd in allowed:
+            prompt = f'skipping `{line}`'
+            utils.cprint(text=prompt, color='yellow', _prefix='')
+        else:
+            await _mbsh.onecmd(line)
+
+
+async def main():
+    args = parse_args()
+    _mbsh = mbsh.MbSh()
+
+    if args.file != None:
+        if not os.path.isfile(args.file):
+            prompt = 'given file was not found'
+            utils.cprint(text=prompt, color='red', _prefix='')
+        else:
+            with open(args.file) as fp:
+                await parse_file(fp, _mbsh)
+    elif args.eval:
+        await _mbsh.onecmd(args.eval)
+    else:
+        await _mbsh.cmdloop()
 
 if __name__ == '__main__':
-    _mbsh = mbsh.MbSh()
-    event_loop = asyncio.get_event_loop()
-
-    event_loop.run_until_complete(_mbsh.cmdloop())
+    asyncio.get_event_loop().run_until_complete(main())
